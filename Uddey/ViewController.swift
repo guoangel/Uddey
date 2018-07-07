0316c7c81b2f024667de84069039cc9254d2fa33
Angel <angel@jonathans-mbp.lan>
2018-07-08 00:55:59 +0800

    Initial Commit

---
+++ Uddey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uddey
+//
+//  Created by Angel on 8/7/18.
+//  Copyright © 2018 Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
